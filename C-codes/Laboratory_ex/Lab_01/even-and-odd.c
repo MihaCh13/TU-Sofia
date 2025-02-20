7186ebb76d2d15d0cd6c6de6f131e71d3d3653fc
Miha Ch <148248932+MihaCh13@users.noreply.github.com>
2025-02-20 09:48:43 +0200

    Create even-and-odd.c

---
+++ C-codes/Laboratory_ex/Lab_01/even-and-odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    if (x % 2 == 0){
+        printf("chetno e");
+    }
+    else{
+        printf("nechetno e");
+    }
+    
+    return 0;
+}
