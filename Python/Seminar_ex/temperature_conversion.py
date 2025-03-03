63783cc395a4dfe1c0d51d3e6ed39cf38de3c569
Miha Ch <148248932+MihaCh13@users.noreply.github.com>
2025-03-03 16:26:56 +0200

    Create temperature_conversion.py

---
+++ Python/Seminar_ex/temperature_conversion.py
@@ -0,0 +1,5 @@
+# Преобразуване на температура от Целзий във Фаренхайт
+
+temperature = float(input("Моля въведете моментната температура в целзии: "))
+farenhait = float(1.8 * temperature + 32)
+print(f"Моментната температура {temperature}°C отговаря на {farenhait}°F")
